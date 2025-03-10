9743b12de5c1c8c43553c51fe3b992fd4a6d859b
Manali Kale <manaliakale@gmail.com>
2025-03-09 21:24:26 -0600

    Final version

--- components/Main.tsx
+++ components/Main.tsx
@@ -32,7 +32,7 @@ const Main = () => {
 
   // Update mouse position
   useEffect(() => {
-    const handleMouseMove = (e) => {
+    const handleMouseMove = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
       setMousePosition({ x: e.clientX, y: e.clientY });
     };
 
