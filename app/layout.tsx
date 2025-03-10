fc7d9c0e4dac49e4ebf7cce8162ebf7ace025f10
Manali Kale <manaliakale@gmail.com>
2025-03-10 11:16:14 -0600

    Final changes for favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import { Metadata } from 'next';
 export const metadata: Metadata = {
   title: "Manali Kale | Portfolio",
   description: "Manali Kale Portfolio",
+  icons: {
+    icon: '/favicon.ico',
+  },
 };
 
 export default function RootLayout({
@@ -13,6 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="scroll-smooth">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body>
         {children}
       </body>
